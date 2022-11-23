c2b86b170db2255d540e568e482401ae2e51b052
VESSE Leo <leo.vesse@telecomnancy.eu>
2022-11-23 19:29:32 +0100

    Reorganized everything please respect

--- PROJET/app.py
+++ PROJET/app.py
@@ -12,7 +12,7 @@ DATABASE = 'project.db'
 
 def get_db():
 
-    db = getattr(g, '_database', None)
+    db = getattr(g, DATABASE, None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
@@ -22,7 +22,7 @@ def get_db():
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, DATABASE, None)
     if db is not None:
         db.close()
 
