7ab42265dd5d4e7a1683ddd307e615daed25dc97
Leruez <thomas.leruez@telecomnancy.eu>
2023-01-10 19:21:31 +0100

    Commentaires

--- PROJET/app.py
+++ PROJET/app.py
@@ -202,96 +202,109 @@ def renvoyer_prod():
 
 @app.route('/publications', methods=["GET", "POST"])
 def publications():
+    # Si l'utilisateur n'est pas connecté, il ne peut pas accéder à cette page : on le redirige vers l'accueil.
     if "id_utilisateur" not in session:
         return redirect("/")
     userid = session["id_utilisateur"]
     conn = get_db()
     cur = conn.cursor()
     if request.method == "POST":
+        # Si l'utilisateur a posté une publication, on l'insère dans la bd
         if request.form.get('submessage') != None:
             cur.execute("INSERT INTO publications (posterid, date, message) VALUES (?, datetime('now','localtime'), ?)",
                         (userid, request.form['message']))
             conn.commit()
+        # Si l'utilisateur a posté un commentaire, on l'insère dans la bd
         elif request.form.get('subcomm') != None:
             cur.execute("INSERT INTO commentaires (postid, userid, commentaire, date) VALUES (?, ?, ?, datetime('now','localtime'))",
                         (request.form['postid'], userid, request.form['messagecomm']))
             conn.commit()
     cur.execute("SELECT publications.id, posterid, strftime('%d/%m/%Y à %H:%M', date), message, pseudo FROM publications JOIN utilisateurs ON utilisateurs.id = posterid WHERE posterid IN (SELECT ? UNION SELECT following FROM follow WHERE follower = ?) ORDER BY publications.id DESC LIMIT 15", (userid, userid))
+    # On récupère les publications qui doivent s'afficher sur le fil d'actualité de l'utilisateur
     publis = cur.fetchall()
-    Lcomm = []
+    Lpublis = []  # Initialisation de la liste des publications avec ses commentaires
     for p in publis:
         cur.execute(
             "SELECT commentaires.id, userid, commentaire, strftime('%d/%m/%Y à %H:%M', date), pseudo FROM commentaires JOIN utilisateurs ON utilisateurs.id = userid WHERE postid = ? ORDER BY commentaires.id DESC", (p[0],))
-        Lcomm.append(p + (cur.fetchall(),))
-    return render_template('publications.html', publis=Lcomm)
+        # On ajoute à la liste la publication p avec ses commentaires
+        Lpublis.append(p + (cur.fetchall(),))
+    return render_template('publications.html', publis=Lpublis)
 
 
 @app.route('/messagerie/<int:readid>', methods=["GET", "POST"])
 @app.route('/messagerie/', methods=["GET", "POST"])
 def messagerie(readid=0):
+    # Si l'utilisateur n'est pas connecté, il ne peut pas accéder à cette page : on le redirige vers l'accueil.
     if "id_utilisateur" not in session:
         return redirect("/")
     userid = session["id_utilisateur"]
-    msgread = None
+    msgread = None  # Initialisation des paramètres du message lu, va être modifié s'il y en a effectivement un
     conn = get_db()
     cur = conn.cursor()
+    # Si l'utilisateur a souhaité supprimer des messages de sa boîte de réception
     if request.form.get('suppmsg') != None:
-        Lsuppr = []
+        Lsuppr = []  # Initialisation de la liste des suppressions
         for k in list(request.form.keys()):
-            if k.isdigit():
-                Lsuppr.append(int(k))
+            if k.isdigit():  # Si la clé est un entier naturel (le "name" des checkboxes)
+                Lsuppr.append(int(k))  # On ajoute à la liste des suppressions
         Lsupprstr = "(" + str(Lsuppr)[1:-1] + ")"
-        # Moche, mais pas trouvé d'autre moyen avec le IN
+        # Moche, mais pas trouvé d'autre moyen avec le IN, et normalement pas de faille SQL car uniquement des chiffres
         cur.execute("UPDATE messages SET suppr = 1 WHERE id IN " + Lsupprstr)
         conn.commit()
+    # Si l'utilisateur veut supprimer le message qu'il était en train de lire
     if request.form.get('suppmsglecture') != None:
         cur.execute("UPDATE messages SET suppr = 1 WHERE id = ?",
                     (request.form['readid'],))
         conn.commit()
-    if readid > 0:
+    if readid > 0:  # Si l'utilisateur a cliqué sur un message à lire
+        # On met le message qui va être lu par l'utilisateur dans l'état "lu"
         cur.execute(
             "UPDATE messages SET read = 1 WHERE id = ? AND receiver = ?", (readid, userid))
         conn.commit()
         cur.execute("SELECT messages.id, sender, subject, strftime('%d/%m/%Y à %H:%M', date), pseudo, body FROM messages JOIN utilisateurs ON utilisateurs.id = sender WHERE receiver = ? AND suppr = 0 AND messages.id = ? ORDER BY messages.id DESC", (userid, readid))
+        # On récupère les données du message que l'utilisateur veut lire
         msgread = cur.fetchone()
     cur.execute("SELECT messages.id, sender, read, subject, strftime('%d/%m/%Y à %H:%M', date), pseudo FROM messages JOIN utilisateurs ON utilisateurs.id = sender WHERE receiver = ? AND suppr = 0 ORDER BY messages.id DESC LIMIT 15", (userid,))
-    messages = cur.fetchall()
+    messages = cur.fetchall()  # On récupère la boîte de réception de l'utilisateur
     return render_template('messagerie.html', messages=messages, msgread=msgread)
 
 
 @app.route('/ecrire', methods=["GET", "POST"])
 def ecrire():
+    # Si l'utilisateur n'est pas connecté, il ne peut pas accéder à cette page : on le redirige vers l'accueil.
     if "id_utilisateur" not in session:
         return redirect("/")
     userid = session["id_utilisateur"]
     if request.form.get('objet') != None:
         objet = request.form['objet']
     else:
-        objet = "Pas d'objet"
+        objet = "Pas d'objet"  # Objet par défaut
     if request.form.get('dest') != None:
         dest = request.form['dest']
     else:
-        dest = ""
+        dest = ""  # Destinateur par défaut
     if request.form.get('corps') != None:
         corps = request.form['corps']
     else:
-        corps = ""
+        corps = ""  # Corps nul par défaut
+    # Initialisation d'un éventuel message d'erreur. S'il n'y en a pas, on restera sur None.
     erreur = None
-    tentenvoi = None
+    tentenvoi = None  # Si l'utilisateur vient d'envoyer un message
 
     if request.form.get('envoyer') != None:
         conn = get_db()
         cur = conn.cursor()
         tentenvoi = True
-        cur.execute("SELECT id FROM utilisateurs WHERE pseudo = ?",
-                    (request.form.get('dest'),))
+        cur.execute("SELECT id FROM utilisateurs WHERE pseudo = ?", (request.form.get(
+            'dest'),))  # On récupère l'id de l'utilisateur qui correspond au pseudo entré
         iddesttuple = cur.fetchone()
-        if iddesttuple != None:
+        if iddesttuple != None:  # Si on a effectivement un utilisateur dont le pseudo correspond
             iddest = iddesttuple[0]
             cur.execute("INSERT INTO messages (sender, receiver, subject, body, date) VALUES (?, ?, ?, ?, datetime('now','localtime'))",
-                        (userid, iddest, objet, corps))
+                        (userid, iddest, objet, corps))  # On insère le message dans la base de données (correspond donc à l'envoi)
             conn.commit()
         else:
+            # Erreur si le pseudo entré ne correspond à aucun pseudo dans la base de données.
             erreur = "Le pseudo spécifié n'existe pas."
     return render_template('ecrire.html', objet=objet, dest=dest, corps=corps, erreur=erreur, tentenvoi=tentenvoi)
 
